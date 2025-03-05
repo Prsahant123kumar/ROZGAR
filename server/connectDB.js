eba61c5341ed2d50083f84856367e15c3f0dcca8
ROZGAR <kumarparshant12540@gmail.com>
2025-03-05 11:49:48 +0530

    connected mongodb compass to atlas

--- server/connectDB.js
+++ server/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const mongoDB=async()=>{
-    mongoose.connect('mongodb://localhost:27017/RAZGOR')
+    mongoose.connect('mongodb+srv://prashant12540kumar:prashant@test-pro-db.m8qvl.mongodb.net/?retryWrites=true&w=majority&appName=test-pro-db')
     console.log("MongoDb connected");
     mongoose.connection.once('open', async () => {
         try {
